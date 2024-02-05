dcb19911e7125cfb8a71d2b2f46cb5461626fa65
Arinzelight <arinzelight2@gmail.com>
2024-02-05 22:24:44 +0100

    pycodestyle check

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -18,7 +18,7 @@ def api_status():
     return jsonify(response)
 
 
-@app_views.route('/api/v1/stats', methods=['GET'])
+@app_views.route('/stats', methods=['GET'])
 def get_stats():
     '''
     Retrieves the number of each objects by type.
@@ -32,4 +32,3 @@ def get_stats():
         'users': storage.count('User')
     }
     return jsonify(stats)
-
