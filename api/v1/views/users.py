79240be42c68725f6cd8b42871ef7665caa934e4
Arinzelight <arinzelight2@gmail.com>
2024-02-06 22:33:40 +0100

    users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,80 +1,137 @@
 #!/usr/bin/python3
 '''
-    RESTful API for class User
+Create a new view for User objects - handles all default RESTful API actions
 '''
-from flask import Flask, jsonify, abort, request
+
+# Import necessary modules
+from flask import abort, jsonify, request
+# Import the User model
+from models.user import User
 from api.v1.views import app_views
 from models import storage
-from models.user import User
 
 
+# Route for retrieving all User objects
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
-def get_users():
+def get_all_users():
     '''
-        return all user objects in json form
+    retrieves the list of all User objects
     '''
-    user_list = [u.to_dict() for u in storage.all('User').values()]
-    return jsonify(user_list)
+    # Get all User objects from the storage and convert them to dictionaries
+    users = storage.all(User).values()
+    return jsonify([user.to_dict() for user in users])
 
 
+# Route for retrieving a specific User object by ID
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
-def get_user_id(user_id):
+def get_user(user_id):
     '''
-        return user with given id using http verb GET
+    Retrieves a User object
     '''
-    user = storage.get("User", user_id)
-    if user is None:
+    # Get the User object with the given ID from the storage
+    user = storage.get(User, user_id)
+    if user:
+        # Return the User object in JSON format
+        return jsonify(user.to_dict())
+    else:
+        # Return 404 error if the User object is not found
         abort(404)
-    return jsonify(user.to_dict())
 
 
-@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+# Route for deleting a specific User object by ID
+@app_views.route('/users/<user_id>', methods=['DELETE'])
 def delete_user(user_id):
     '''
-        delete user obj given user_id
+    Deletes a User object
     '''
-    user = storage.get("User", user_id)
-    if user is None:
+    # Get the User object with the given ID from the storage
+    user = storage.get(User, user_id)
+    if user:
+        # Delete the User object from the storage and save changes
+        storage.delete(user)
+        storage.save()
+        # Return an empty JSON with 200 status code
+        return jsonify({}), 200
+    else:
+        # Return 404 error if the User object is not found
         abort(404)
-    user.delete()
-    storage.save()
-    return jsonify({}), 200
 
 
+# Route for creating a new User object
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def create_user():
     '''
-        create new user obj
+    Creates a User object
     '''
+    # Check if the request data is in JSON format
     if not request.get_json():
-        return jsonify({"error": "Not a JSON"}), 400
-    elif "email" not in request.get_json():
-        return jsonify({"error": "Missing email"}), 400
-    elif "password" not in request.get_json():
-        return jsonify({"error": "Missing password"}), 400
-    else:
-        obj_data = request.get_json()
-        obj = User(**obj_data)
-        obj.save()
-        return jsonify(obj.to_dict()), 201
+        # Return 400 error if the request data is not in JSON format
+        abort(400, 'Not a JSON')
+
+    # Get the JSON data from the request
+    data = request.get_json()
+    if 'email' not in data:
+        # Return 400 error if 'email' key is missing in the JSON data
+        abort(400, 'Missing email')
+    if 'password' not in data:
+        # Return 400 error if 'password' key is missing in the JSON data
+        abort(400, 'Missing password')
+
+    # Create a new User object with the JSON data
+    user = User(**data)
+    # Save the User object to the storage
+    user.save()
+    # Return the newly created User object in JSON format with 201 status code
+    return jsonify(user.to_dict()), 201
 
 
+# Route for updating an existing User object by ID
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def update_user(user_id):
     '''
-        update existing user object
+    Updates a User object
     '''
-    if not request.get_json():
-        return jsonify({"error": "Not a JSON"}), 400
-    obj = storage.get("User", user_id)
-    if obj is None:
+    # Get the User object with the given ID from the storage
+    user = storage.get(User, user_id)
+    if user:
+        # Check if the request data is in JSON format
+        if not request.get_json():
+            # Return 400 error if the request data is not in JSON format
+            abort(400, 'Not a JSON')
+
+        # Get the JSON data from the request
+        data = request.get_json()
+        ignore_keys = ['id', 'email', 'created_at', 'updated_at']
+        # Update the attributes of the User object with the JSON data
+        for key, value in data.items():
+            if key not in ignore_keys:
+                setattr(user, key, value)
+
+        # Save the updated User object to the storage
+        user.save()
+        # Return the updated User object in JSON format with 200 status code
+        return jsonify(user.to_dict()), 200
+    else:
+        # Return 404 error if the User object is not found
         abort(404)
-    obj_data = request.get_json()
-    ignore = ("id", "email", "created_at", "updated_at")
-    for k in obj_data.keys():
-        if k in ignore:
-            pass
-        else:
-            setattr(obj, k, obj_data[k])
-    obj.save()
-    return jsonify(obj.to_dict()), 200
+
+
+# Error Handlers:
+@app_views.errorhandler(404)
+def not_found(error):
+    '''
+    Returns 404: Not Found
+    '''
+    # Return a JSON response for 404 error
+    response = {'error': 'Not found'}
+    return jsonify(response), 404
+
+
+@app_views.errorhandler(400)
+def bad_request(error):
+    '''
+    Return Bad Request message for illegal requests to the API
+    '''
+    # Return a JSON response for 400 error
+    response = {'error': 'Bad Request'}
+    return jsonify(response), 400
