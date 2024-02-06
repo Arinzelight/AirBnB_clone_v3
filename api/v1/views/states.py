604e17d8c88f3b35c505e67d71fd656a96c033ee
Arinzelight <arinzelight2@gmail.com>
2024-02-06 23:16:17 +0100

    states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,13 +1,9 @@
-#!/usr/bin/python3
-"""
-Create a new view for State objects - handles all default RESTful API actions.
-"""
-
 # Import necessary modules
 from flask import abort, jsonify, request
 from models.state import State
 from api.v1.views import app_views
 from models import storage
+
 
 # Route for retrieving all State objects
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
@@ -20,6 +16,7 @@ def get_all_states():
     # Convert objects to dictionaries and jsonify the list
     state_list = [state.to_dict() for state in states]
     return jsonify(state_list)
+
 
 # Route for retrieving a specific State object by ID
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
@@ -35,6 +32,7 @@ def get_state(state_id):
     else:
         # Return 404 error if the State object is not found
         abort(404)
+
 
 # Route for deleting a specific State object by ID
 @app_views.route('/states/<state_id>', methods=['DELETE'])
@@ -53,6 +51,7 @@ def delete_state(state_id):
     else:
         # Return 404 error if the State object is not found
         abort(404)
+
 
 # Route for creating a new State object
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
@@ -76,6 +75,7 @@ def create_state():
     state.save()
     # Return the newly created State object in JSON format with 201 status code
     return jsonify(state.to_dict()), 201
+
 
 # Route for updating an existing State object by ID
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
@@ -106,6 +106,7 @@ def update_state(state_id):
         # Return 404 error if the State object is not found
         abort(404)
 
+
 # Error Handlers:
 
 @app_views.errorhandler(404)
@@ -116,6 +117,7 @@ def not_found(error):
     # Return a JSON response for 404 error
     response = {'error': 'Not found'}
     return jsonify(response), 404
+
 
 @app_views.errorhandler(400)
 def bad_request(error):
